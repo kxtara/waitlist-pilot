a9acc60d2b696ddcb3088be48ec524b0092acba1
Kiara Hoheb <kiarahoheb@gmail.com>
2026-05-17 20:03:25 -0400

    update redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,12 @@ export const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 
 export const Route = createFileRoute("/")({
-  beforeLoad: () => {
-    throw redirect({ to: "/admin" });
+  beforeLoad: ({ location }) => {
+    if(location.pathname === "/") {
+      throw redirect({ 
+        to: "/admin",
+        replace: true,
+      });
+    }
   },
 });
